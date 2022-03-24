7717c801d4b973e0078cc175e2dab18299a633ea
L-archeveque <valentin.larcheveque@etu.umontpellier.fr>
2022-03-24 20:23:47 +0100

    2nd commit functions

--- AFS_functions.R
+++ AFS_functions.R
@@ -1,11 +1,34 @@
 ##Formules random utilisables
 ## Distance géodésique: 2*arcos(sum(sqrt(p*q)))
-install.packages(geosphere)
-N=100
+
+
 dist_geod<- function(x,y){
   return (2*acos(sum(sqrt(x*y))))
-
 }
+
+Hellingerizer<-function(x){
+
+  return (sign(x)*sqrt(abs(x)))
+}
+
+
+Nuage_X<-function(t,mI){
+  ##mI proba sur I mJ proba sur J
+
+  I<-length(mI)
+  X<-t%*%diag(1/sqrt(mI))
+return(X)
+}
+
+Nuage_Y<-function(t,mJ){
+  ##mI proba sur I mJ proba sur J
+
+  J<-length(mJ)
+  Y<-diag(1/sqrt(mJ))%*%t
+  return(Y)
+}
+
+
 turn_to_freq<-function(x){
   return(x/sum(x))
 }
@@ -33,23 +56,7 @@ I_alpha<-function (p,q,alpha){
 dist_Hellinger<-function(p,q){
   return (sum((sqrt(p)-sqrt(q))^2))
 }
-usethis::use_git_config(user.name = "L-archeveque", user.email = "valentin.larcheveque@etu.umontpellier.fr")
-credentials::set_github_pat("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")
-usethis::create_github_token()
-edit_r_environ()
-usethis::edit_r_environ()
-usethis::git_sitrep()
 
-sum(sqrt(x*y))
-library("pracma")
-install.packages("pracma")
 
-install.packages("distrEx")
-library("distrEx")
 
-p<-rep(1/6,6)
-q<-(1:6)/sum(1:6)
-dist_Hellinger(p,q)
-dist_Hellinger(p,p)
-B<-matrix((9:1),3,3,byrow=T)/sum(1:9)
-dist_Hellinger(B,A)
+
