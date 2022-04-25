a5c90bafe4a8d9f5c4cef33961a8b0414bbec1cb
L-archeveque <valentin.larcheveque@etu.umontpellier.fr>
2022-04-25 14:40:40 +0200

    Commit 4 functions and Applied

--- AFS_functions.R
+++ AFS_functions.R
@@ -12,79 +12,72 @@ Hellingerizer<-function(x){
 }
 
 
-Nuage_X<-function(t,mI){
-
-  I<-length(mI)
-  X<-diag(1/sqrt(mI))%*%t
-return(X)
+Nuage_X<-function(t,mI=poids_lignes(t)){
+  Z<-depistage(t)
+  X<-Z@mat
+  if (Z@ili != c(0)){
+    linull<-Z@ili
+    n<-length(linull)
+    for (x in 1:n){ i<-linull[x]
+    mI<-mI[-i]}
+  }
+  X<-diag(1/sqrt(abs(mI)))%*%X
+  return(X)
 }
-
-Nuage_Y<-function(t,mJ){
-
-
-  Y<-t%*%diag(1/sqrt(mJ))
+Nuage_Y<-function(t,mJ=poids_colonne(t)){
+  Z<-depistage(t)
+  Y<-Z@mat
+  if (Z@icol != c(0)){
+    conull<-Z@icol
+    n<-length(conull)
+    for (x in 1:n){j<-conull[x]
+    mJ<-mJ[-j]}}
+  Y<-Y%*%diag(1/sqrt(abs(mJ)))
   return(Y)
 }
-
-
-turn_to_freq<-function(x){
-  return(x/sum(abs(x)))
-}
-poids_lignes<-function(f){
-
-  I<-length(f[,1])
-
-  mI<-rep(0,I)
-
-  for (i in 1:I){
-    mI[i]<-sum(f[i,])
-}
-
-  return(mI)
-}
-
 poids_colonne<-function(f){
   J<-length(f[1,])
   mJ<-rep(0,J)
   for (j in 1:J){
     mJ[j]<-sum(f[,j])
-
-
-  }
+                    }
   return(mJ)
 }
-depistage<-function(t){
-  J<-length(f[1,])
-  I<-length(f[,1])
-  F2<-f
-  for(j in 1:J){
-    if(sum(f[,j]==0)){F2<-f[,-j]}}
-  for ( i in 1:I){
-    if(sum(f[i,]==0)){F2<-f[-i,]}
-  }
+poids_lignes<-function(f){
+  J<-length(f[,1])
+  mI<-rep(0,J)
+  for (j in 1:J){
+    mI[j]<-sum(f[j,])}
+  return(mI)}
+    
+    
+    
+setClass("matrice_d",representation(mat="matrix",icol="numeric",ili="numeric"))
+         
+         depistage<-function(f){
+           J<-length(f[1,])
+           I<-length(f[,1])
+           F2<-f
+           ind_li<-c()
+           ind_col<-c()
+           for(j in 1:J){
+             if(sum(f[,j])==0){F2<-f[,-j]
+             
+             ind_col<-c(ind_col,j)}}
+           for (i in 1:I){
+             if(sum(f[i,])==0){F2<-f[-i,]
+             ind_li<-c(ind_li,j)}}
+           if(is.null(ind_col)){ind_col<-c(0)}
+           if(is.null(ind_li)){ind_li<-c(0)}
+           r=new("matrice_d",mat=F2,ili=ind_li,icol=ind_col)
+           return(r)
+         }
 
-return(F2)
-}
 
-
-
-Profil_ligne<-function(x){
-  for (i in 1:length(x[,1])){
-    x[i,]<-turn_to_freq(x[i,])
-
-  }
-  return(x)
-
-}
-Profil_colonne<-function(x){
-  for (i in 1:length(x[1,])){
-    x[,i]<-turn_to_freq(x[,i])
-  }
-  return(x)
-
-}
-
-
+         turn_to_freq<-function(x){
+           return(x/sum(abs(x)))
+         }
+         
 I_alpha<-function (p,q,alpha){
   return ((1/(alpha-1))*log2((sum(((p^alpha)/(q^alpha))*p))))
 
